0e164c34539f15aab3ee96c45c8478ba085c0a2b
Juliano <julianopaulo.santos@hotmail.com>
2019-10-31 14:30:16 -0300

    Medida de seguraça com funções do php contra sql injection

--- estabelecimento/excluir_pedido.php
+++ estabelecimento/excluir_pedido.php
@@ -3,7 +3,9 @@
     include DBAPI;
     $id = $_GET['id'];
     $db = open_database();
-    $sql = "update pedido set status = 0 where id = ".$id;
-    $exec = $db->query($sql);
+    $sql = "update pedido set status = 0 where id=?";
+    $exec = $db->prepare($sql);
+    $exec->bind_param("i",$id);
+    $exec->execute();
     header('location:telafunc.php');
 ?>
