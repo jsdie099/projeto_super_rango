3296141c6910e5ad262262b1fcc47900de8f15be
Juliano <julianopaulo.santos@hotmail.com>
2019-10-31 01:16:27 -0300

    Medida contra sql injection usando funções do php

--- cliente/cancelar.php
+++ cliente/cancelar.php
@@ -2,8 +2,11 @@
     require_once "config.php";
     require_once DBAPI;
 
+    $id = base64_decode($_GET['id']);
     $db = open_database();
-    $sql = "delete from pedido where id =".base64_decode($_GET['id']);
-    $exec = $db->query($sql);
-    $id = base64_encode($_GET['id']);
-    header('location:historico.php?id='.$id);
+    $sql = "delete from pedido where id =?";
+    $exec = $db->prepare($sql);
+    $exec->bind_param("i",$id);
+    $exec->execute();
+    
+    header('location:historico.php?id='.base64_encode($_GET['id']));
